77e1461e7322ec4ad021fe3daa8159c8209261e4
ttopeor <ttoper@udel.edu>
2023-10-09 02:03:40 -0700

    imporved python sdk

--- PythonSDK/STR400_SDK/str400.py
+++ PythonSDK/STR400_SDK/str400.py
@@ -1,53 +1,79 @@
-import requests
+import websocket
 import json
+import time
+import threading
+from queue import Queue
+from queue import Empty
 
 
 class STR400:
-    def __init__(self, base_url):
-        self.base_url = base_url
+    def __init__(self, host='localhost', port=8080):
+        self.uri = f"ws://{host}:{port}/api/ws"
+        self.ws = websocket.WebSocketApp(self.uri, on_message=self.on_message)
+        self.message_queue = Queue()
+        self.connected = False
 
-    def send_request(self, action, payload):
+        thread = threading.Thread(target=self.run_forever)
+        thread.daemon = True
+        thread.start()
+
+        time.sleep(2)  # 休眠一段时间确保WebSocket连接建立
+
+    def on_message(self, ws, message):
+        self.message_queue.put(json.loads(message))
+
+    def send_request(self, action, payload=None):
         data = {
             "action": action,
             "payload": payload
         }
-        response = requests.post(self.base_url, json=data)
-        return response.json()
+        self.ws.send(json.dumps(data))
+
+        # 返回从服务器收到的下一条消息
+        while True:
+            try:
+                return self.message_queue.get(timeout=5)
+            except Empty:
+                return None
+
+    def run_forever(self):
+        self.ws.run_forever()
 
     def enable(self):
-        return self.send_request("Enable", None)
+        self.send_request("Enable")
 
     def disable(self):
-        return self.send_request("Disable", None)
+        self.send_request("Disable")
 
     def stop(self):
-        return self.send_request("Stop", None)
+        self.send_request("Stop")
 
     def back_to_zero(self):
         payload = {"type": "BackToZeroTask"}
-        return self.send_request("SetTask", payload)
+        self.send_request("SetTask", payload)
 
     def external_position_control(self, task, args=None):
         payload = {"type": task, "args": args}
-        return self.send_request("SetTask", payload)
+        self.send_request("SetTask", payload)
 
     def real_time_position_control(self, values):
         action = "RealTimePositionControl"
-        return self.send_request(action, values)
+        self.send_request(action, values)
 
     def wscript(self, script, repeatCount=1):
         args = {"script": script, "repeatCount": repeatCount}
         payload = {"type": "WScriptTask", "args": args}
-        return self.send_request("SetTask", payload)
+        self.send_request("SetTask", payload)
 
     def movej(self, angles):
         payload = {"type": "MoveJTask", "args": angles}
-        return self.send_request("SetTask", payload)
+        self.send_request("SetTask", payload)
 
     def movel(self, positions):
         payload = {"type": "MoveLTask", "args": positions}
-        return self.send_request("SetTask", payload)
+        self.send_request("SetTask", payload)
 
     def moves(self, positions):
         payload = {"type": "MoveSTask", "args": positions}
-        return self.send_request("SetTask", payload)
+        self.send_request("SetTask", payload)
+
--- PythonSDK/example.py
+++ PythonSDK/example.py
@@ -1,15 +1,15 @@
 from STR400_SDK.str400 import STR400
 import time
 
-robot = STR400(base_url="ws://localhost:8080/api/ws")
+robot = STR400(host='localhost', port=8080)
 
 # Example usage
 response = robot.enable()
 print(response)
 
-time.sleep(500)
+time.sleep(1)
 
 # MoveJ command
-angles = [0, 0, 90, 0, 90, 0]
+angles = [0, 0, 90, 0, 90, 0, 6]
 response = robot.movej(angles)
 print(response)
